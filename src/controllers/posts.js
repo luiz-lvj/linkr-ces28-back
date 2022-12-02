a87d544e83578aa5009158f338754ac4b4b358b4
Jose Andre <jose.andrenunes5@gmail.com>
2022-12-02 07:03:52 -0300

    first push

--- src/controllers/posts.js
+++ src/controllers/posts.js
@@ -1,5 +1,5 @@
 import { getLikesFromPost } from "../services/likes.js";
-import { createPostservice, getPostService } from "../services/posts.js";
+import { createPostservice, getPostService, getPostByIdService, deletePostService, editPostService } from "../services/posts.js";
 import { getUserById } from "../services/signIn.js";
 import { getUserByToken } from "../services/token.js";
 
@@ -92,6 +92,94 @@ export async function getPostsController(req, res){
 
 
     }catch(err){
+        console.log(err);
+        return res.sendStatus(500);
+    }
+}
+
+// edit post if created by user
+
+export async function editPostController(req, res){
+    try{
+        const text = req.body.text;
+        const id = req.body.id;
+        const auth = req.headers.authorization;
+        const token = String(auth).split(" ")[1];
+        console.log(token)
+        
+        const user = await getUserByToken(token);
+        console.log(user);
+        if(user == null){
+            return res.sendStatus(404).json({"message":"usuario não encontrado"});
+        }
+
+        const post = await getPostByIdService(id);
+
+        if(post.user_id != user.id){
+            return res.sendStatus(403).json({"message":"Você não pode editar esse post"});
+        }
+
+        const bodyPost = {
+            text: text,
+            id: id
+        }
+        const postId = await editPostService(bodyPost);
+        if(postId == 0){
+            console.log("Não salvou o post!");
+            return res.sendStatus(500);
+        }
+        const likes = await getLikesFromPost(postId);
+        const ans = {
+            id: postId,
+            text: text,
+            link: link,
+            linkTitle: "título do link",
+            linkDescription: "Descrição do link",
+            linkImage: "Imagem do link",
+            user: {
+                id: user.id,
+                username: user.username,
+                avatar: user.avatar
+            },
+            likes: likes
+        }
+        return res.send(ans);
+
+    } catch(err){
+        console.log(err);
+        return res.sendStatus(500);
+    }
+}
+
+// delete post if created by user
+
+export async function deletePostController(req, res){
+    try{
+        const id = req.body.id;
+        const auth = req.headers.authorization;
+        const token = String(auth).split(" ")[1];
+        console.log(token)
+        
+        const user = await getUserByToken(token);
+        console.log(user);
+        if(user == null){
+            return res.sendStatus(404).json({"message":"usuario não encontrado"});
+        }
+
+        const post = await getPostByIdService(id);
+
+        if(post.user_id != user.id){
+            return res.sendStatus(403).json({"message":"Você não pode deletar esse post"});
+        }
+
+        const postId = await deletePostService(id);
+        if(postId == 0){
+            console.log("Não deletou o post!");
+            return res.sendStatus(500);
+        }
+        return res.sendStatus(200);
+
+    } catch(err){
         console.log(err);
         return res.sendStatus(500);
     }
--- src/services/posts.js
+++ src/services/posts.js
@@ -31,3 +31,42 @@ export async function getPostService(){
         return [];
     }
 }
+
+
+// edit post only text
+
+export async function editPostService(post){
+    try{
+        const text = post.text;
+        const id = post.id;
+        const postRes = await connection.query(`UPDATE posts SET text = $1 WHERE id = $2 RETURNING id`,[text, id]);
+        return postRes.rows[0].id;
+    }catch(err){
+        console.log(err);
+        return 0;
+    }
+}
+
+// delete post
+
+export async function deletePostService(id){
+    try{
+        const postRes = await connection.query(`DELETE FROM posts WHERE id = $1 RETURNING id`,[id]);
+        return postRes.rows[0].id;
+    }catch(err){
+        console.log(err);
+        return 0;
+    }
+}
+
+// get post by id
+
+export async function getPostByIdService(id){
+    try{
+        const post = await connection.query(`SELECT * FROM posts WHERE id = $1`,[id]);
+        return post.rows[0];
+    }catch(err){
+        console.log(err);
+        return [];
+    }
+}
