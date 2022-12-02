df49a11e02e319dba4d4f4f3d3aece81ffc965aa
Luiz <lvj253@gmail.com>
2022-12-02 03:12:38 -0300

    db and setup

---
+++ src/server.js
@@ -0,0 +1,15 @@
+import "./setup.js";
+
+import express from "express";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const port = +process.env.PORT || 4000;
+
+
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}.`);
+});
